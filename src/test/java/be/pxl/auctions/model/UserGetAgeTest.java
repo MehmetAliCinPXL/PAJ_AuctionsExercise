e0ef4544458cd021e2e42049d64e752df097881e
MehmetAliCinPXL <MehmetAliCin.pxl.be>
2021-04-01 10:51:51 +0200

    Added age test

--- src/test/java/be/pxl/auctions/model/UserGetAgeTest.java
+++ src/test/java/be/pxl/auctions/model/UserGetAgeTest.java
@@ -1,23 +1,37 @@
 package be.pxl.auctions.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class UserGetAgeTest {
+import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserGetAgeTest {
+	private static final int YEARS = 15;
+	private User user;
+
+	@BeforeEach
+	public void setup(){
+		user = UserBuilder.anUser().build();
+	}
 
 	@Test
 	public void returnsCorrectAgeWhenHavingBirthdayToday() {
-		// TODO implement test
+		user.setDateOfBirth(LocalDate.now());
+		assertEquals(YEARS, user.getAge());
 	}
 
 	@Test
 	public void returnsCorrectAgeWhenHavingBirthdayTomorrow() {
-		// TODO implement test
+		user.setDateOfBirth(LocalDate.now().plusYears(YEARS));
+		assertEquals(YEARS, user.getAge());
 	}
 
 	@Test
 	public void returnsCorrectAgeWhenBirthdayWasYesterday() {
-		// TODO implement test
+		user.setDateOfBirth(LocalDate.now().minusYears(YEARS));
+		assertEquals(YEARS, user.getAge());
 	}
 
 }
